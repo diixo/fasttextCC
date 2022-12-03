258be4448daedeeb5efc2b118786e51de004f7f8
diixo <anatolii.podoba@gmail.com>
2022-12-03 12:28:23 +0200

    update

--- tests/dictionary-main.cc
+++ tests/dictionary-main.cc
@@ -1,12 +1,16 @@
 
 #include "dictionary.h"
 #include "args.h"
+
 #include <memory>
+#include <iostream>
+#include <fstream>
 
 int main()
 {
    auto args = std::make_shared<fasttext::Args>();
    args->input = "train-data.txt";
    fasttext::Dictionary dictionary(args, 557);
+   dictionary.readFromFile(std::ifstream(args->input), nullptr);
    return 0;
 }
